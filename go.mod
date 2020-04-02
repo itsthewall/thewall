9601fc0db65b95067b65c67418d2c6c9ce43a1e4
Obi Symons <symons.obi@gmail.com>
2020-04-02 18:16:50 +1100

    Parse emails
    - Very dodgy
    - Doesn't seem to like certain types of emails (e.g. Attachments, Forwards etc).

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/itsthewall/thewall
 
 go 1.13
 
-require github.com/lib/pq v1.3.0
+require (
+	github.com/DusanKasan/Parsemail v0.0.0-20200228075709-6e5c9f904641
+	github.com/lib/pq v1.3.0
+)
--- mail.go
+++ mail.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"io"
+	"log"
+	"mime"
+	"mime/multipart"
 	"net/http"
-	"os"
+	"net/mail"
+
+	parsemail "github.com/DusanKasan/Parsemail"
 )
+
+type Post struct { //Simple representation of emails for now.
+	Title string
+	Body  string
+	Email *mail.Address
+}
 
 func handleMail(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
@@ -12,9 +25,55 @@ func handleMail(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, "Must use POST.")
 		return
 	}
+	//Always respond with a 200 status code to prevent send grid from
+	//resending emails.
+	defer w.WriteHeader(200)
+	defer io.WriteString(w, "Received.")
 
-	io.Copy(os.Stdout, r.Body)
+	//TODO Verify that the request is coming from send grid.
 
-	w.WriteHeader(200)
-	io.WriteString(w, "OK")
+	rawEmail, err := getRawEmail(r)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	email, err := parsemail.Parse(rawEmail)
+	if err != nil {
+		log.Printf("Error parsing email: %v", err)
+		return
+	}
+
+	if len(email.From) == 0 {
+		return
+	}
+
+	post := Post{
+		Title: email.Subject,
+		Body:  email.TextBody,
+		Email: email.From[0],
+	}
+
+	fmt.Printf("%v: %v\n%v", post.Title, post.Email.String(), post.Body)
+}
+
+func getRawEmail(r *http.Request) (io.Reader, error) {
+	//Parse Grid gives us a multipart encoded POST request. Which we need to extract
+	//the raw email from.
+	_, params, err := mime.ParseMediaType(r.Header.Get("Content-Type"))
+	if err != nil {
+		log.Println(err)
+	}
+	mr := multipart.NewReader(r.Body, params["boundary"])
+	for {
+		p, err := mr.NextPart()
+		if err == io.EOF {
+			return nil, errors.New("No email field found in body.")
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		if p.FormName() == "email" {
+			return p, nil
+		}
+	}
 }
