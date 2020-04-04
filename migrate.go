09c74b7fba7f204baecf647b2e4d8bb182e247ad
Harrison Shoebridge <harrison@theshoebridges.com>
2020-04-04 13:46:39 +1100

    Make sure migrations are only ran once

--- migrate.go
+++ migrate.go
@@ -18,18 +18,18 @@ var migrations []migration = []migration{
 	{
 		name: "init schema",
 		up: `
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE users (
 		id SERIAL PRIMARY KEY,
 		name VARCHAR UNIQUE NOT NULL,
 		email VARCHAR UNIQUE NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS blocks (
+CREATE TABLE blocks (
 		id SERIAL PRIMARY KEY,
 		title VARCHAR
 );
 
-CREATE TABLE IF NOT EXISTS posts (
+CREATE TABLE posts (
 		id SERIAL PRIMARY key,
 
 		block_id INTEGER REFERENCES blocks(id),
@@ -43,20 +43,15 @@ CREATE TABLE IF NOT EXISTS posts (
 	{
 		name: "add created_at timestamps",
 		up: `
-DO $$
-BEGIN
-	ALTER TABLE users ADD COLUMN created_at TIMESTAMP DEFAULT NOW();
-	ALTER TABLE posts ADD COLUMN created_at TIMESTAMP DEFAULT NOW();
-	ALTER TABLE blocks ADD COLUMN created_at TIMESTAMP DEFAULT NOW();
-EXCEPTION WHEN duplicate_column THEN
-	RAISE NOTICE 'Field already exists. Ignoring...';
-END$$;
+ALTER TABLE users ADD COLUMN created_at TIMESTAMP DEFAULT NOW();
+ALTER TABLE posts ADD COLUMN created_at TIMESTAMP DEFAULT NOW();
+ALTER TABLE blocks ADD COLUMN created_at TIMESTAMP DEFAULT NOW();
 		`,
 	},
 	{
 		name: "tokens table for auth",
 		up: `
-CREATE TABLE IF NOT EXISTS tokens (
+CREATE TABLE tokens (
 	id SERIAL PRIMARY KEY,
 	token VARCHAR UNIQUE NOT NULL
 );
@@ -65,15 +60,52 @@ CREATE TABLE IF NOT EXISTS tokens (
 }
 
 func migrate(db *sql.DB) error {
-	log.Println("Applying migrations")
-	for _, m := range migrations {
-		_, err := db.Exec(m.up)
+	checkQuery := `SELECT EXISTS ( SELECT * FROM information_schema.tables WHERE information_schema.tables.table_name = 'migrations' );`
+	row := db.QueryRow(checkQuery)
 
+	exists := false
+	if err := row.Scan(&exists); err != nil {
+		return fmt.Errorf("couldn't check if `migrations` table existed: %w", err)
+	}
+
+	if !exists {
+		log.Println("Creating migrations table")
+
+		initQuery := `CREATE TABLE migrations ( id SERIAL PRIMARY KEY, name VARCHAR NOT NULL, created_at TIMESTAMP DEFAULT NOW() );`
+		_, err := db.Exec(initQuery)
+
+		if err != nil {
+			return fmt.Errorf("couldn't create `migrations` table: %w", err)
+		}
+	}
+
+	log.Println("Applying migrations")
+	alreadyDoneQuery := `SELECT EXISTS (SELECT * FROM migrations WHERE name = $1 LIMIT 1 );`
+	for _, m := range migrations {
+		exists := false
+		row := db.QueryRow(alreadyDoneQuery, m.name)
+		if err := row.Scan(&exists); err != nil {
+			return err
+		}
+
+		if exists {
+			log.Println("Skipping migration:", m.name)
+
+			continue
+		}
+
+		_, err := db.Exec(m.up)
 		if err != nil {
 			return fmt.Errorf("failed in migration %s: %w", m.name, err)
 		}
 
-		log.Println("Applied migration", m.name)
+		log.Println("Applied migration:", m.name)
+
+		saveQuery := `INSERT INTO migrations (name) VALUES ($1);`
+		_, err = db.Exec(saveQuery, m.name)
+		if err != nil {
+			return fmt.Errorf("couldn't save migration %s: %w", m.name, err)
+		}
 	}
 
 	log.Println("Finished applying migrations")
